0b827cb79b5c818a63bbb3eb5ef2e2ae9bedef7a
Madzzcientizt <seankirbyreales@gmail.com>
2026-04-11 09:27:13 +0800

    added Game List and 12 game pages

---
+++ src/data/gamesCatalog.js
@@ -0,0 +1,565 @@
+window.gameCatalog = [{
+        title: "Clair Obscur: Expedition 33",
+        genre: "RPG",
+        image: "../../public/posters/E33_poster.jpg",
+        href: "./gamesList/expedition33.html"
+    },
+    {
+        title: "Persona 5 Royal",
+        genre: "RPG",
+        image: "../../public/posters/persona5Royal_poster.jpg",
+        href: "./gamesList/persona5Royal.html"
+    },
+    {
+        title: "Final Fantasy 16",
+        genre: "RPG",
+        image: "../../public/posters/finalFantasy16_poster.jpg",
+        href: "./gamesList/finalFantasy16.html"
+    },
+    {
+        title: "Monster Hunter World",
+        genre: "RPG",
+        image: "../../public/posters/monsterHunterWorld_poster.jpg",
+        href: "./gamesList/monsterHunterWorld.html"
+    },
+    {
+        title: "Octopath Traveler 2",
+        genre: "RPG",
+        image: "../../public/posters/octopathTraveler2_poster.jpg",
+        href: "./gamesList/octopathTraveler2.html"
+    },
+    {
+        title: "Slay the Spire 2",
+        genre: "Card Game",
+        image: "../../public/posters/slayTheSpire2_poster.jpg",
+        href: "./gamesList/slayTheSpire2.html"
+    },
+    {
+        title: "Red Dead Redemption 2",
+        genre: "Action",
+        image: "../../public/posters/redDeadRedemption2_poster.jpg",
+        href: "./gamesList/redDeadRedemption2.html"
+    },
+    {
+        title: "The Legend of Zelda: Tears of the Kingdom",
+        genre: "Action",
+        image: "../../public/posters/TLOZ_poster.jpg",
+        href: "./gamesList/TLOZ.html"
+    },
+    {
+        title: "Super Mario Odyssey",
+        genre: "Platformer",
+        image: "../../public/posters/superMarioOdyssey_poster.jpg",
+        href: "./gamesList/superMarioOdyssey.html"
+    },
+    {
+        title: "Metaphor: ReFantazio",
+        genre: "RPG",
+        image: "../../public/posters/metaphor_poster.jpg",
+        href: "./gamesList/metaphor.html"
+    },
+    {
+        title: "Persona 3 Reload",
+        genre: "RPG",
+        image: "../../public/posters/persona3Reload_poster.jpg",
+        href: "./gamesList/persona3Reload.html"
+    },
+    {
+        title: "Final Fantasy 7 Rebirth",
+        genre: "RPG",
+        image: "../../public/posters/finalFantasy7Rebirth_poster.jpg",
+        href: "./gamesList/finalFantasy7Rebirth.html"
+    },
+    {
+        title: "Half-Life",
+        genre: "Shooter",
+        image: "../../public/posters/halfLife_poster.jpg",
+        href: "./gamesList/halfLife.html"
+    },
+    {
+        title: "Counter-Strike",
+        genre: "Shooter",
+        image: "../../public/posters/counterStrike_poster.jpg",
+        href: "./gamesList/counterStrike.html"
+    },
+    {
+        title: "Portal",
+        genre: "Platformer",
+        image: "../../public/posters/portal_poster.jpg",
+        href: "./gamesList/portal.html"
+    },
+    {
+        title: "Team Fortress 2",
+        genre: "Shooter",
+        image: "../../public/posters/teamFortress2_poster.jpg",
+        href: "./gamesList/teamFortress2.html"
+    },
+    {
+        title: "osu!",
+        genre: "Rhythm",
+        image: "../../public/posters/osu_poster.jpg",
+        href: "./gamesList/osu.html"
+    },
+    {
+        title: "Deadlock",
+        genre: "Shooter",
+        image: "../../public/posters/deadlock_poster.jpg",
+        href: "./gamesList/deadlock.html"
+    },
+    {
+        title: "Quaver",
+        genre: "Rhythm",
+        image: "../../public/posters/quaver_poster.jpg",
+        href: "./gamesList/quaver.html"
+    },
+    {
+        title: "A Dance of Fire and Ice",
+        genre: "Rhythm",
+        image: "../../public/posters/aDanceOfFireAndIce_poster.jpg",
+        href: "./gamesList/aDanceOfFireAndIce.html"
+    },
+    {
+        title: "Muse Dash",
+        genre: "Rhythm",
+        image: "../../public/posters/museDash_poster.jpg",
+        href: "./gamesList/museDash.html"
+    },
+    {
+        title: "Hatsune Miku: Project DIVA",
+        genre: "Rhythm",
+        image: "../../public/posters/projectDiva_poster.jpg",
+        href: "./gamesList/projectDiva.html"
+    },
+    {
+        title: "Elden Ring",
+        genre: "RPG",
+        image: "../../public/posters/eldenRing_poster.jpg",
+        href: "./gamesList/eldenRing.html"
+    },
+    {
+        title: "Cyberpunk 2077",
+        genre: "RPG",
+        image: "../../public/posters/cyberpunk2077_poster.jpg",
+        href: "./gamesList/cyberpunk2077.html"
+    },
+    {
+        title: "Ghost of Tsushima",
+        genre: "Action",
+        image: "../../public/posters/ghostOfTsushima_poster.jpg",
+        href: "./gamesList/ghostOfTsushima.html"
+    },
+    {
+        title: "Assassin's Creed",
+        genre: "Action",
+        image: "../../public/posters/assassinsCreed_poster.jpg",
+        href: "./gamesList/assassinsCreed.html"
+    },
+    {
+        title: "Dying Light",
+        genre: "Horror",
+        image: "../../public/posters/dyingLight_poster.jpg",
+        href: "./gamesList/dyingLight.html"
+    },
+    {
+        title: "Resident Evil",
+        genre: "Horror",
+        image: "../../public/posters/residentEvil_poster.jpg",
+        href: "./gamesList/residentEvil.html"
+    },
+    {
+        title: "Warframe",
+        genre: "Shooter",
+        image: "../../public/posters/warframe_poster.jpg",
+        href: "./gamesList/warframe.html"
+    },
+    {
+        title: "Need for Speed",
+        genre: "Racing",
+        image: "../../public/posters/needForSpeed_poster.jpg",
+        href: "./gamesList/needForSpeed.html"
+    },
+    {
+        title: "Terraria",
+        genre: "Sandbox",
+        image: "../../public/posters/terraria_poster.jpg",
+        href: "./gamesList/terraria.html"
+    },
+    {
+        title: "Stardew Valley",
+        genre: "Simulation",
+        image: "../../public/posters/stardewValley_poster.jpg",
+        href: "./gamesList/stardewValley.html"
+    },
+    {
+        title: "Minecraft",
+        genre: "Sandbox",
+        image: "../../public/posters/minecraft_poster.jpg",
+        href: "./gamesList/minecraft.html"
+    },
+    {
+        title: "League of Legends",
+        genre: "MOBA",
+        image: "../../public/posters/leagueOfLegends_poster.jpg",
+        href: "./gamesList/leagueOfLegends.html"
+    },
+    {
+        title: "Geometry Dash",
+        genre: "Rhythm",
+        image: "../../public/posters/geometryDash_poster.jpg",
+        href: "./gamesList/geometryDash.html"
+    },
+    {
+        title: "Friday Night Funkin'",
+        genre: "Rhythm",
+        image: "../../public/posters/fnf_poster.jpg",
+        href: "./gamesList/fnf.html"
+    },
+    {
+        title: "Genshin Impact",
+        genre: "RPG",
+        image: "../../public/posters/genshinImpact_poster.jpg",
+        href: "./gamesList/genshinImpact.html"
+    },
+    {
+        title: "Subnautica",
+        genre: "Sandbox",
+        image: "../../public/posters/subnautica_poster.jpg",
+        href: "./gamesList/subnautica.html"
+    },
+    {
+        title: "Dead Space",
+        genre: "Horror",
+        image: "../../public/posters/deadSpace_poster.jpg",
+        href: "./gamesList/deadSpace.html"
+    },
+    {
+        title: "Overwatch 2",
+        genre: "Shooter",
+        image: "../../public/posters/overwatch2_poster.jpg",
+        href: "./gamesList/overwatch2.html"
+    },
+    {
+        title: "Valorant",
+        genre: "Shooter",
+        image: "../../public/posters/valorant_poster.jpg",
+        href: "./gamesList/valorant.html"
+    },
+    {
+        title: "Hades II",
+        genre: "Action",
+        image: "../../public/posters/hades2_poster.jpg",
+        href: "./gamesList/hades2.html"
+    },
+    {
+        title: "Sea of Thieves",
+        genre: "Action",
+        image: "../../public/posters/seaOfThieves_poster.jpg",
+        href: "./gamesList/seaOfThieves.html"
+    },
+    {
+        title: "Detroit: Become Human",
+        genre: "Interactive Drama",
+        image: "../../public/posters/detroitBecomeHuman_poster.jpg",
+        href: "./gamesList/detroitBecomeHuman.html"
+    },
+    {
+        title: "Mirror's Edge",
+        genre: "Action",
+        image: "../../public/posters/mirrorsEdge_poster.jpg",
+        href: "./gamesList/mirrorsEdge.html"
+    },
+    {
+        title: "Bloodborne",
+        genre: "RPG",
+        image: "../../public/posters/bloodborne_poster.jpg",
+        href: "./gamesList/bloodborne.html"
+    },
+    {
+        title: "Kingdom Hearts",
+        genre: "RPG",
+        image: "../../public/posters/kingdomHearts_poster.jpg",
+        href: "./gamesList/kingdomHearts.html"
+    },
+    {
+        title: "Crash Bandicoot",
+        genre: "Platformer",
+        image: "../../public/posters/crashBandicoot_poster.jpg",
+        href: "./gamesList/crashBandicoot.html"
+    },
+    {
+        title: "Raft",
+        genre: "Sandbox",
+        image: "../../public/posters/raft_poster.jpg",
+        href: "./gamesList/raft.html"
+    },
+    {
+        title: "Party Animals",
+        genre: "Action",
+        image: "../../public/posters/partyAnimals_poster.jpg",
+        href: "./gamesList/partyAnimals.html"
+    },
+    {
+        title: "Ghost Recon",
+        genre: "Shooter",
+        image: "../../public/posters/ghostRecon_poster.jpg",
+        href: "./gamesList/ghostRecon.html"
+    },
+    {
+        title: "Yu-Gi-Oh! Master Duel",
+        genre: "Card Game",
+        image: "../../public/posters/masterDuel_poster.jpg",
+        href: "./gamesList/masterDuel.html"
+    },
+    {
+        title: "Umamusume: Pretty Derby",
+        genre: "Simulation",
+        image: "../../public/posters/umamusume_poster.jpg",
+        href: "./gamesList/umamusume.html"
+    },
+    {
+        title: "Arknights: Endfield",
+        genre: "RPG",
+        image: "../../public/posters/endfield_poster.jpg",
+        href: "./gamesList/endfield.html"
+    },
+    {
+        title: "Fallout",
+        genre: "RPG",
+        image: "../../public/posters/fallout_poster.jpg",
+        href: "./gamesList/fallout.html"
+    },
+    {
+        title: "Ace Attorney",
+        genre: "Visual Novel",
+        image: "../../public/posters/aceAttorney_poster.jpg",
+        href: "./gamesList/aceAttorney.html"
+    },
+    {
+        title: "Rhythm Heaven",
+        genre: "Rhythm",
+        image: "../../public/posters/rhythmHeaven_poster.jpg",
+        href: "./gamesList/rhythmHeaven.html"
+    },
+    {
+        title: "Undertale",
+        genre: "RPG",
+        image: "../../public/posters/undertale_poster.jpg",
+        href: "./gamesList/undertale.html"
+    },
+    {
+        title: "Sekiro: Shadows Die Twice",
+        genre: "Action",
+        image: "../../public/posters/sekiro_poster.jpg",
+        href: "./gamesList/sekiro.html"
+    },
+    {
+        title: "Seven Days to Die",
+        genre: "Survival",
+        image: "../../public/posters/7daysToDie_poster.jpg",
+        href: "./gamesList/7daysToDie.html"
+    },
+    {
+        title: "Content Warning",
+        genre: "Co-op",
+        image: "../../public/posters/contentWarning_poster.jpg",
+        href: "./gamesList/contentWarning.html"
+    },
+    {
+        title: "War Thunder",
+        genre: "Simulation",
+        image: "../../public/posters/warThunder_poster.jpg",
+        href: "./gamesList/warThunder.html"
+    },
+    {
+        title: "World War Z",
+        genre: "Shooter",
+        image: "../../public/posters/worldWarZ_poster.jpg",
+        href: "./gamesList/worldWarZ.html"
+    },
+    {
+        title: "Left 4 Dead 2",
+        genre: "FPS",
+        image: "../../public/posters/left4Dead2_poster.jpg",
+        href: "./gamesList/left4Dead2.html"
+    },
+    {
+        title: "Crossfire",
+        genre: "FPS",
+        image: "../../public/posters/crossfire_poster.jpg",
+        href: "./gamesList/crossfire.html"
+    },
+    {
+        title: "It Takes Two",
+        genre: "Adventure",
+        image: "../../public/posters/itTakesTwo_poster.jpg",
+        href: "./gamesList/itTakesTwo.html"
+    },
+    {
+        title: "Grand Theft Auto V",
+        genre: "Action",
+        image: "../../public/posters/gtaV_poster.jpg",
+        href: "./gamesList/gtaV.html"
+    },
+    {
+        title: "Prototype",
+        genre: "Action",
+        image: "../../public/posters/prototype_poster.jpg",
+        href: "./gamesList/prototype.html"
+    },
+    {
+        title: "Toy Story",
+        genre: "Platformer",
+        image: "../../public/posters/toyStory_poster.jpg",
+        href: "./gamesList/toyStory.html"
+    },
+    {
+        title: "Disco Elysium",
+        genre: "RPG",
+        image: "../../public/posters/discoElysium_poster.jpg",
+        href: "./gamesList/discoElysium.html"
+    },
+    {
+        title: "NieR: Automata",
+        genre: "RPG",
+        image: "../../public/posters/nierAutomata_poster.jpg",
+        href: "./gamesList/nierAutomata.html"
+    },
+    {
+        title: "Signalis",
+        genre: "Horror",
+        image: "../../public/posters/signalis_poster.jpg",
+        href: "./gamesList/signalis.html"
+    },
+    {
+        title: "Killer7",
+        genre: "Action",
+        image: "../../public/posters/killer7_poster.jpg",
+        href: "./gamesList/killer7.html"
+    },
+    {
+        title: "Who's Lila?",
+        genre: "Adventure",
+        image: "../../public/posters/whosLila_poster.jpg",
+        href: "./gamesList/whosLila.html"
+    },
+    {
+        title: "Hotline Miami",
+        genre: "Shooter",
+        image: "../../public/posters/hotlineMiami_poster.jpg",
+        href: "./gamesList/hotlineMiami.html"
+    },
+    {
+        title: "Return of the Obra Dinn",
+        genre: "Puzzle",
+        image: "../../public/posters/obraDinn_poster.jpg",
+        href: "./gamesList/obraDinn.html"
+    },
+    {
+        title: "Cruelty Squad",
+        genre: "Action",
+        image: "../../public/posters/crueltySquad_poster.jpg",
+        href: "./gamesList/crueltySquad.html"
+    },
+    {
+        title: "Hotel Dusk: Room 215",
+        genre: "Visual Novel",
+        image: "../../public/posters/hotelDusk_poster.jpg",
+        href: "./gamesList/hotelDusk.html"
+    },
+    {
+        title: "The Silver Case",
+        genre: "Visual Novel",
+        image: "../../public/posters/silverCase_poster.jpg",
+        href: "./gamesList/silverCase.html"
+    },
+    {
+        title: "Little Nightmares",
+        genre: "Platformer",
+        image: "../../public/posters/littleNightmares_poster.jpg",
+        href: "./gamesList/littleNightmares.html"
+    },
+    {
+        title: "Dragon Nest",
+        genre: "MMORPG",
+        image: "../../public/posters/dragonNest_poster.jpg",
+        href: "./gamesList/dragonNest.html"
+    },
+    {
+        title: "The Spike",
+        genre: "Sports",
+        image: "../../public/posters/theSpike_poster.jpg",
+        href: "./gamesList/theSpike.html"
+    },
+    {
+        title: "Doki Doki Literature Club",
+        genre: "Visual Novel",
+        image: "../../public/posters/ddlc_poster.jpg",
+        href: "./gamesList/ddlc.html"
+    },
+    {
+        title: "The Division",
+        genre: "Shooter",
+        image: "../../public/posters/theDivision_poster.jpg",
+        href: "./gamesList/theDivision.html"
+    },
+    {
+        title: "DJMAX Respect V",
+        genre: "Rhythm",
+        image: "../../public/posters/djmax_poster.jpg",
+        href: "./gamesList/djmax.html"
+    },
+    {
+        title: "Project Sekai",
+        genre: "Rhythm",
+        image: "../../public/posters/projectSekai_poster.jpg",
+        href: "./gamesList/projectSekai.html"
+    },
+    {
+        title: "Bomb Rush Cyberfunk",
+        genre: "Platformer",
+        image: "../../public/posters/bombRushCyberfunk_poster.jpg",
+        href: "./gamesList/bombRushCyberfunk.html"
+    },
+    {
+        title: "R.E.P.O.",
+        genre: "Horror",
+        image: "../../public/posters/repo_poster.jpg",
+        href: "./gamesList/repo.html"
+    },
+    {
+        title: "Until Then",
+        genre: "Adventure",
+        image: "../../public/posters/untilThen_poster.jpg",
+        href: "./gamesList/untilThen.html"
+    },
+    {
+        title: "Dispatch",
+        genre: "Strategy",
+        image: "../../public/posters/dispatch_poster.jpg",
+        href: "./gamesList/dispatch.html"
+    },
+    {
+        title: "PEAK",
+        genre: "Exploration",
+        image: "../../public/posters/peak_poster.jpg",
+        href: "./gamesList/peak.html"
+    },
+    {
+        title: "Hearthstone",
+        genre: "Card Game",
+        image: "../../public/posters/hearthstone_poster.jpg",
+        href: "./gamesList/hearthstone.html"
+    },
+    {
+        title: "Death Stranding",
+        genre: "RPG",
+        image: "../../public/posters/deathStranding_poster.jpg",
+        href: "./gamesList/deathStranding.html"
+    },
+    {
+        title: "Lies of P",
+        genre: "RPG",
+        image: "../../public/posters/liesOfP_poster.jpg",
+        href: "./gamesList/liesOfP.html"
+    }
+];
